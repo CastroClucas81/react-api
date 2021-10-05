e0e3426d203a67e7dae707dcee5f357b3c61434a
Lucas Cardoso de Castro <sky.lucas2013@gmail.com>
2021-10-04 22:50:20 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import api from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  //armazenando os filmes
+  state = {
+    filmes: [],
+  }
+
+  //componentDidMount é executado assim que a página é renderizada
+  async componentDidMount() {
+    //pegando os filmes... está vazio pois eu nao estou retornando nada
+    const resposta = await api.get('star%20wars  ');
+    //setar os valores que eu receber em filmes
+    this.setState({
+      filmes: resposta.data
+    })
+  }
+
+  render() {
+    //atribundo os registros a uma constante    
+    const { filmes } = this.state;
+
+    return (
+      <div>
+        <h1> Listar filmes </h1>
+        {filmes.map(filme => (
+          <li key={filme.show.id}>
+            <h2> <strong>Título do filme: </strong> {filme.show.name} {filme.show.url}</h2>
+          </li>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
